05ddef449f18c010e21140c5682ce3e20c8efb8f
nkatz01 <nkatz01@dcs.bbk.ac.uk>
2020-06-03 19:28:37 +0100

    done mock two

--- Q8/Program.cs
+++ Q8/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Timers;
-//using System.Windows.Forms.Timer;
+ 
 namespace Q8
 {
     class Program
@@ -25,74 +26,61 @@ namespace Q8
     {
 
         public static System.Timers.Timer aTimer;
-        public double ReadTemperature()//Object source, ElapsedEventArgs e
+        public double ReadTemperature() 
         {
-            if (Read() == -300)
+            if (Read() == -300.0)
                 throw new Exception();
             else
             {
-             //   Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
-                //          e.SignalTime);
+           
                  return Read();
             }
         }
 
         public double Read()
         {
-            return -300;
-            //  throw new NotImplementedException();
+       
+             throw new NotImplementedException();
         }
         public void Monitor(double low, double high)
         {
-             
-              aTimer = new System.Timers.Timer(1000);
+            int seconds = 0;
+
+            aTimer = new System.Timers.Timer(1000);
             aTimer.Elapsed += delegate
             {
-               int seconds = 0 ; 
+                try {
                 double n = Read();
-                   Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",  DateTime.Now);
-                if (n != -300)
-                {
-                    seconds = 0;
-
-                    if (n > high || n < low)
+                    if (n != -300.0)
                     {
-                        Console.Error.WriteLine("Temp is out of range");
+                        seconds = 0;
+
+                        if (n > high || n < low)
+                        {
+                            Console.Error.WriteLine("Temp is out of range");
+                        }
                     }
+
                 }
 
-                else if (n == -300)
-                {
+                catch (Exception e) {
+
+               
                     seconds++;
                 }
+                   Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",  DateTime.Now);
 
                 if (seconds>60)
                     Console.Error.WriteLine("Sensor not responsive");
 
             };
-                //(sender, e) => ReadTemperature(sender, e) ;
+                
             aTimer.AutoReset = true;
             aTimer.Enabled = true;
            
 
 
-
-            //System.Timers.Timer tmr = new System.Timers.Timer();
-            //tmr.Elapsed += (sender, args) => ReadTemperature();
-            //tmr.AutoReset = true;
-            //tmr.Interval = 1000;
-            //tmr.Start();
-
-
-
-            //var startTimeSpan = TimeSpan.Zero;  //https://stackoverflow.com/questions/13019433/calling-a-method-every-x-minutes
-            //var periodTimeSpan = TimeSpan.FromSeconds(1);
-
-            //var timer = new System.Threading.Timer((e) =>
-            //{
-            //    ReadTemperature();
-            //    Console.WriteLine(DateTime.Now);
-            //}, null, startTimeSpan, periodTimeSpan);
+ 
 
 
         }
